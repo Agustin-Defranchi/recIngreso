40cfb5737390ff91ff4dc6b55eafbe447ac89860
Agustin-Defranchi <agustin.i.defranchi@gmail.com>
2021-03-10 10:21:44 -0300

    falta el 3

--- tres.js
+++ tres.js
@@ -23,10 +23,16 @@ Curso de ingreso UTN FRA
 	let marca;
 	let cantidad;
 	let precio ;
-	let descuento;
-	let importeFinal;
-	let impuesto;
 	let salir;
+	let precioTotal;
+	let precioUnidad;
+	let contfelipe=0;
+	let contArgentina=0;
+	let contiluminatis=0;
+	let descuentoArgentina;
+	let descuentoFeliepe;
+	let precioDescFelip;
+	let precioDescArgentina;
 
 	do
 	{
@@ -42,6 +48,44 @@ Curso de ingreso UTN FRA
 			cantidad = parseInt(prompt("ingrese una cantidad dentro de los limites(de 1 en adelante"));
 		}
 
+		marca = prompt("ingrese la marca de la lampara (felipelamparas - argentinaluz - illuminatis)");
+		while(marca != "felipelamparas" && marca != "argentinaluz"&& marca != "illuminatis")
+		{
+			marca = prompt("ingrese la marca de la lampara (felipelamparas - argentinaluz - illuminatis)");
+		}	
+
+		precio = parseInt(prompt("ingrese el precio individual (de 1 en adelante)"));
+		while(precio <= 0 || isNaN(precio))
+		{
+			precio = parseInt(prompt("ingrese el precio individual dentro de los limites(de 1 en adelante"));
+		}
+
+		precioUnidad = precio * cantidad;
+		precioTotal += precioUnidad;
+
+		if(marca=="felipelamparas")
+		{
+			contfelipe++;
+			if(cantidad>5)
+			{
+				descuentoFeliepe = precioUnidad * 10/100;
+				precioDescFelip = precioUnidad - descuentoFeliepe;
+
+			}
+		}
+		else if(marca=="argentinaluz")
+		{
+			contArgentina++;
+			if(cantidad>=3)
+			{
+				descuentoArgentina = precioUnidad * 5/100;
+				precioDescArgentina=precioUnidad - descuentoArgentina;
+			}
+		}
+		else
+		{
+			contiluminatis++;
+		}
 
 
 		
@@ -50,6 +94,8 @@ Curso de ingreso UTN FRA
 		salir=prompt("desea salir? (ingrese s para salir)");
 	}while(salir != "s");
 
+	alert("la empreza recauda un total de: "+precioTotal);
+
 }
 
 /*
@@ -57,8 +103,10 @@ Curso de ingreso UTN FRA
 Enunciado:
 Ferrete lamparas 2.0 El empleado del negocio ingresa los siguientes datos de cada venta, teniendo en cuenta que tenemos varias ventas:
 nombre del cliente
-cantidad de lamparas marca (FelipeLamparas - ArgentinaLuz - Illuminatis)
-precio por unidad total de la compra: dato que se calcula.
+cantidad de lamparas 
+marca (FelipeLamparas - ArgentinaLuz - Illuminatis)
+precio por unidad 
+total de la compra: dato que se calcula.
 Se sabe que si la marca es FelipeLamparas y la cantidad de lamparas supera las 5 unidades, 
 se aplica un descuento del 10% sobre la compra Si la marca es ArgentinaLuz y compra 3 o mas unidades,
  el descuento es del 5%. Ningun otro caso aplica descuento. al terminar la carga de todas las ventas,la empresa desea saber:
